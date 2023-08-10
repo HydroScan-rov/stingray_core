2558015562613a903e71246173c53f32ca953d96
Vladislav Plotnikov <vladik1209@gmail.com>
2023-08-10 19:47:52 +0300

    circuit -> loop

--- src/stingray_communication/include/messages/config.h
+++ src/stingray_communication/include/messages/config.h
@@ -14,7 +14,7 @@ struct RequestConfigMessage : public AbstractMessage
 
     uint8_t flags; // [0]thrusters_on, [1]reset_imu, [2]reset_depth, [3]rgb_light_on, [4]lower_light_on,
     uint8_t stab_flags; // stab [0]march, [1]lag, [2]depth, [3]roll, [4]pitch, [5]yaw, [6]thrusters_on, [6]reset_imu
-    uint8_t current_circuit; // current contour: [0]march, [1]lag, [2]depth, [3]roll, [4]pitch, [5]yaw
+    uint8_t current_loop; // current contour: [0]march, [1]lag, [2]depth, [3]roll, [4]pitch, [5]yaw
 
     float_t march;
     float_t lag;
